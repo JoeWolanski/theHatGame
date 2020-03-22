d99d1abdd486bb29480de0985d1d909aa1c57dc0
System Administrator <root@josephwolanski.fritz.box>
2020-03-22 17:28:43 +0100

    pausing due to mongo issue

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
+
 const { check, validationResult } = require('express-validator');
+
 const router = express.Router();
 
 router.get('/', (req, res) => {
@@ -11,23 +13,12 @@ router.post('/',
     check('name')
       .isLength({ min: 1 })
       .withMessage('Please enter a name'),
-    check('item')
+    check('email')
       .isLength({ min: 1 })
-      .withMessage('Please enter an item'),
+      .withMessage('Please enter an email'),
   ],
-   (req, res) => {
-    const errors = validationResult(req);
-
-    if (errors.isEmpty()) {
-      res.send('Thank you for your registration!');
-    } else {
-      res.render('form', {
-        title: 'Registration form',
-        errors: errors.array(),
-        data: req.body,
-      });
-    }
-  }
-);
+  (req, res) => {
+  res.render('form', { title: 'Registration form' });
+});
 
 module.exports = router;
